9eb0cd28a2a39095600e7eb8a20097760ef53c3e
Bishoy640 <wolverinebishoy@gmail.com>
2021-01-12 09:42:11 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import shelve
+
+
+locations = {0: {"desc": "You are sitting in front of a computer learning Python",
+                 "exits": {},
+                 "namedExits": {}},
+             1: {"desc": "You are standing at the end of a road before a small brick building",
+                 "exits": {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
+                 "namedExits": {"2": 2, "3": 3, "5": 5, "4": 4, "0": 0}},
+             2: {"desc": "You are at the top of a hill",
+                 "exits": {"N": 5, "Q": 0},
+                 "namedExits": {"5": 5, "0": 0}},
+             3: {"desc": "You are inside a building, a well house for a small stream",
+                 "exits": {"W": 1, "Q": 0},
+                 "namedExits": {"1": 1, "0": 0}},
+             4: {"desc": "You are in a valley beside a stream",
+                 "exits": {"N": 1, "W": 2, "Q": 0},
+                 "namedExits": {"1": 1, "2": 2, "0": 0}},
+             5: {"desc": "You are in the forest",
+                 "exits": {"W": 2, "S": 1, "Q": 0},
+                 "namedExits": {"1": 1, "2": 2, "0": 0}}
+             }
+vocabulary = {"QUIT": "Q",
+              "NORTH": "N",
+              "SOUTH": "S",
+              "EAST": "E",
+              "WEST": "W",
+              "ROAD": "1",
+              "HILL": "2",
+              "BUILDING": "3",
+              "VALLEY": "4",
+              "FOREST": "5"}
+
+loc = 1
+
+while True:
+
+    with shelve.open("locations") as locationShelve:
+
+        availableExits = ", ".join(locationShelve[str(loc)]["exits"].keys())
+        print(locationShelve[str(loc)]["desc"])
+        if loc == 0:
+            break
+        direction = input("Available exits are " + availableExits + " ").upper()
+
+        allExits = locationShelve[str(loc)]["exits"].copy()
+        allExits.update(locationShelve[str(loc)]["namedExits"])
+
+    with shelve.open("vocabulary") as vocabularyShelve:
+        for vocItems in vocabularyShelve:
+            if vocItems in direction:
+                direction = vocabularyShelve[vocItems]
+                break
+        print()
+
+        if direction in allExits:
+            loc = allExits[direction]
+
+        else:
+            print("You cannot go in that direction")
